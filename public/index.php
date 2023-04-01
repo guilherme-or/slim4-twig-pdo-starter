6bc7907ac6bcd9e82f6874ceb2b2845c61f2b91f
Guilherme Rocha <79716932+guilherme-or@users.noreply.github.com>
2023-03-31 23:54:16 -0300

    Pasta bootstrap renomeada para config

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = require __DIR__ . '/../bootstrap/app.php';
+$app = require __DIR__ . '/../config/app.php';
 
-(require __DIR__ . '/../bootstrap/middleware.php')($app);
+(require __DIR__ . '/../config/middleware.php')($app);
 
-(require __DIR__ . '/../bootstrap/routes.php')($app);
+(require __DIR__ . '/../config/routes.php')($app);
 
 $app->run();
