6f90847df87f87c524b62fbe40eb2aed53273d23
Guilherme Rocha <79716932+guilherme-or@users.noreply.github.com>
2023-09-20 13:57:01 -0300

    Session fix

--- app/Middlewares/StartSession.php
+++ app/Middlewares/StartSession.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Middlewares;
 
-use App\Utils\Session;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
@@ -14,7 +13,11 @@ class StartSession implements Middleware
 {
     public function process(Request $request, RequestHandler $handler): Response
     {
-        $request = $request->withAttribute('session', (new Session())->get());
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $request = $request->withAttribute('session', $_SESSION);
         return $handler->handle($request);
     }
 }
